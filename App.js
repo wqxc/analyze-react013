c796491375394186456ac7b06a6826ad407c8002
edz <edz@edzdeMacBook-Air-3.local>
2020-01-10 17:54:03 +0800

    feat: 挂载根组件

--- App.js
+++ App.js
@@ -8,36 +8,37 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.handleClick = this.handleClick.bind(this);
-    this.fileChange = this.fileChange.bind(this);
     this.state = {
       count: 1
     };
   }
   componentWillMount() {
-    // console.log("this is  componentWillMount");
-    this.setState({
-      count: 12
-    });
     this.setState({
       count: 13
     });
   }
   handleClick() {
-    // ReactDOM.render(<World />, document.getElementById("app"));
-    this.handleState();
-    this.handleState();
-    this.handleState();
+    ReactDOM.render(<App />, document.getElementById("app"));
+    // this.handleState();
   }
   handleState() {
-    this.setState({
-      count: this.state.count + 1
-    });
+    this.setState(
+      {
+        count: this.state.count + 1
+      },
+      () => {
+        console.log("state 改变了");
+      }
+    );
   }
 
   render() {
-    // return null;
-    return <div onClick={this.handleClick}>this is app {this.state.count}</div>;
+    return (
+      <div onClick={this.handleClick}>
+        this is app {this.state.count}
+        <Stateless />
+      </div>
+    );
   }
 }
-
 export default App;
--- main.js
+++ main.js
@@ -4,5 +4,5 @@ import ReactDOM from "react-dom";
 import App from "./App.js";
 
 ReactDOM.render(<App />, document.getElementById("app"));
-
+// App.setProps({ name: "Jack" });
 // ReactDOM.render(<App />, document.getElementById("app1"));
